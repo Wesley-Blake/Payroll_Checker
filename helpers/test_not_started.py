e60a3fb2949dbc269c0d98b2d9163bfa5b4c6286
wesblake@proton.me <wesblake@proton.me>
2026-01-26 15:57:43 -0800

    Trying to figure out testing.

--- helpers/test_not_started.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-from not_started import not_started_list
-
-class test_not_started_list:
-    not_started_csv = Path.home().parent
-    def test_not_started_list(self):
-        result = not_started_list(not_started_csv)
-        assert result == {'manager1@mail.com':['1@mail.com','2@mail.com']}
