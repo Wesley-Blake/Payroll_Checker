7a1e2e1e1c1c8558699175710e29f83f4ebed36e
wesblake@proton.me <wesblake@proton.me>
2026-04-16 10:35:17 -0700

    Change logger to make temp files.

--- tests/test_logger_config.py
+++ tests/test_logger_config.py
@@ -4,17 +4,17 @@ from helpers.logger_config import setup_logger
 
 
 class TestLoggerConfig:
-    def test_setup_logger_returns_object(self):
-        logger = setup_logger("test.log")
+    def test_setup_logger_returns_object(self, tmp_path):
+        logger = setup_logger(str(tmp_path / "test.log"))
         assert logger is not None
 
-    def test_setup_logger_is_logger(self):
+    def test_setup_logger_is_logger(self, tmp_path):
         import logging
-        logger = setup_logger("test.log")
+        logger = setup_logger(str(tmp_path / "test.log"))
         assert isinstance(logger, logging.Logger)
 
-    def test_setup_logger_with_different_names(self):
-        logger1 = setup_logger("test1.log")
-        logger2 = setup_logger("test2.log")
+    def test_setup_logger_with_different_names(self, tmp_path):
+        logger1 = setup_logger(str(tmp_path / "test1.log"))
+        logger2 = setup_logger(str(tmp_path / "test2.log"))
         assert logger1 is not None
         assert logger2 is not None
